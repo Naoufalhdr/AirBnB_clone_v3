c8514f351e4f8a0be3b0de7cc446b0f0a095f916
Naoufalhdr <naoufalhdr96@gmail.com>
2024-06-09 17:32:07 +0100

    Task 6: create a new view for 'State' objects that handles all the default RESTful API actions

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     """ Run the Flask server """
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(debug=True, host=host, port=port, threaded=True)
